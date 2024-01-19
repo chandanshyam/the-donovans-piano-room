c8909a556d0d83156ab464484e437e1e2f7b73df
grega1 <adrianogrego1@gmail.com>
2024-01-19 21:50:38 +0000

    initial structure

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,43 @@
-export default function Login() {
+export default function SignUp() {
     return (
-      <>
-      <div>SignUp</div>
-      </>
+      <section>
+        <h2>Sign Up</h2>
+      <form>
+      <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Type your first name"
+        />
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Type your last name"
+        />
+        <label htmlFor="inputEmail">E-mail address:</label>
+        <input
+          type="e-mail"
+          name="inputEmail"
+          placeholder="Type your e-mail:"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Create your password"
+        />
+        <label htmlFor="confirmPassword">Confirm your password</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Create your password"
+        />
+
+        <button>
+          Sign Up</button>
+      </form>
+      </section>
     );
   }
   
