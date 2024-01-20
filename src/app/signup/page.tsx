98897a7e0a53f06618ff474e6b46afe82f31f16c
grega1 <adrianogrego1@gmail.com>
2024-01-20 21:48:21 +0000

    feat: initial style

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,9 @@
 export default function SignUp() {
   return (
+    <section className="flex flex-col items-center justify-center w-96 px-4 py-8 mx-auto md:h-screen ">
     <section className="p-6 space-y-4 md:space-y-6 sm:p-8">
       <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900">Sign Up</h1>
-      <form className="space-y-4 md:space-y-6" action="#">
+      <form className="space-y-4 md:space-y-6 w-80" action="#">
         <div>
           <label
             htmlFor="firstName"
@@ -74,8 +75,9 @@ export default function SignUp() {
             placeholder="Create your password"
           />
         </div>
-        <button>Create an account</button>
+        <button className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-cente">Create an account</button>
       </form>
+    </section>
     </section>
   );
 }
