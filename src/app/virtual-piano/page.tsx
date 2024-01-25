5e7ea35d493ef8303863d7cd343f094820129b5b
Hung Le <hungle2396@gmail.com>
2024-01-25 12:30:26 -0800

    set up the virtual metronome with svgs

--- src/app/virtual-piano/page.tsx
+++ src/app/virtual-piano/page.tsx
@@ -3,11 +3,11 @@ import ShortPiano from "../../assets/svg/ShortPiano.svg";
 
 export default function VirtualPiano() {
   return (
-    <div className="flex h-full justify-center pb-8">
-      <div className="box-border flex w-11/12 flex-col items-center rounded-3xl border-2 border-gray-300 bg-zinc-50 pt-4">
+    <div className="flex h-full items-center justify-center">
+      <div className="box-border flex w-11/12 flex-col items-center rounded-3xl border-2 border-gray-300 bg-zinc-50 py-6">
         <div className="box-border flex h-1/6 w-2/6 items-center justify-center rounded-2xl border-2 border-orange-200 bg-orange-50">
-          <div className="box-border flex h-3/4 w-11/12 items-center justify-center">
-            <div className="box-border inline-flex h-5/6 w-5/6 items-center justify-center rounded-lg border-2 border-orange-500  bg-orange-300 text-xl">
+          <div className="box-border flex h-3/4 w-11/12 items-center justify-center p-4">
+            <div className="box-border inline-flex h-5/6 w-5/6 items-center justify-center rounded-lg border-2 border-orange-500  bg-orange-300 p-2 text-xl">
               Short Piano
             </div>
             <div className="box-border inline-flex h-5/6 w-5/6 items-center justify-center text-xl">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "primary-yellow": "#E9BB18",
+      "secondary-yellow": "#F8D867",
+      "primary-orange": "#E98427",
+      "secondary-orange": "#F6AD69",
+      "primary-green": "#438342",
+      "secondary-green": "#75C973",
+      "primary-purple": "#6F219E",
+      "secondary-purple": "#D8BCFD",
+      "primary-brown": "#59371D",
+      "secondary-brown": "#714B2D",
+      "primary-black": "#1C1A1A",
+      "primary-skin": "#FFF2E5",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
