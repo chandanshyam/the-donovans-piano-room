6e4fd87c27d57c5fb7bb9e608c1fe5b59d7083cc
ZichanYang324 <yzc97324@gmail.com>
2024-02-10 21:28:21 -0500

    change to tailwind css

--- src/app/virtual-piano/page.tsx
+++ src/app/virtual-piano/page.tsx
@@ -2,50 +2,52 @@
 import Image from "next/image";
 import ShortPiano from "../../../public/assets/virtual-piano/ShortPiano.svg";
 import LongPiano from "../../../public/assets/virtual-piano/LongPiano.svg";
-import { useState } from "react";
-import styles from "../../../src/components/virtual-piano.module.css";
+import React, { useState } from "react";
+// import styles from "../../../src/components/virtual-piano.module.css";
 
 
 
 
 export default function VirtualPiano() {
-const [isPiano, setIsPiano] = useState<boolean>(false);
+  const [isPiano, setIsPiano] = useState(false);
+  // Define the base and active classes for buttons
+  const baseButtonClass = "w-full rounded-xl border-0 py-2.5 px-5 cursor-pointer font-noto text-xl font-normal leading-7  text-[#1C1A1A] hover:bg-[#F3C27E]";
+  const activeButtonClass = "bg-[#F3C27E] border-2 border-[#ED9E4A]"; // For conditional styling
 
   return (
     <div className="flex h-full items-center justify-center">
       <div className="box-border flex w-11/12 flex-col items-center rounded-3xl border-2 border-gray-300 bg-zinc-50 py-6">
         <div className="box-border flex h-1/6 w-2/6 items-center justify-center rounded-2xl border-2 border-orange-200 bg-orange-50">
-          <div className="box-border flex h-3/4 w-11/12 p-4">
-           <button
-            className={`${styles.button} ${isPiano === true ? styles.buttonActive : ''}`}
-            onClick={() => setIsPiano(true)}>
-            <div className="box-border inline-flex h-5/6 w-5/6" id="shortPianoBtn">
-              Short Piano
-            </div>
-            </button>
-
+          <div className="box-border flex h-3/4 w-11/12 p-4 items-center justify-center">
             <button
-            className={`${styles.button} ${isPiano === false ? styles.buttonActive : ''}`}
-            onClick={() => setIsPiano(false)}>
-            <div className="box-border inline-flex h-5/6 w-5/6 items-center justify-center" id="longPianoBtn">
-              Long Piano
-            </div>
+              className={`${baseButtonClass} ${isPiano ? activeButtonClass : ''} `}
+              onClick={() => setIsPiano(true)}>
+              <div className="box-border inline-flex h-5/6 w-5/6 items-center justify-center cursor-pointer" id="shortPianoBtn">
+                Short Piano
+              </div>
+            </button>
+            <button
+              className={`${baseButtonClass} ${!isPiano ? activeButtonClass : ''} `}
+              onClick={() => setIsPiano(false)}>
+              <div className="box-border inline-flex h-5/6 w-5/6 items-center justify-center" id="longPianoBtn">
+                Long Piano
+              </div>
             </button>
           </div>
         </div>
-        {isPiano === true ? (
+        {isPiano ? (
           <Image 
-          priority  
-          className="h-5/6 w-11/12 pt-8"
-          src={ShortPiano}  
-          alt="short piano" 
+            priority  
+            className="h-5/6 w-11/12 pt-8"
+            src={ShortPiano}  
+            alt="short piano" 
           />
         ) : (
           <Image 
-          priority  
-          className="h-5/6 w-11/12 pt-8"
-          src={LongPiano}  
-          alt="long piano" 
+            priority  
+            className="h-5/6 w-11/12 pt-8"
+            src={LongPiano}  
+            alt="long piano" 
           />
         )}
       </div>
