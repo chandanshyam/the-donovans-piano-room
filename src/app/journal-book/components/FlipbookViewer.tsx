64668675aaa9edc0308def2b148d51aecc540dac
ZichanYang324 <yzc97324@gmail.com>
2024-03-23 01:11:16 -0400

    format the code

--- src/app/journal-book/components/FlipbookViewer.tsx
+++ src/app/journal-book/components/FlipbookViewer.tsx
@@ -1,28 +1,31 @@
-'use client';
-// components/FlipbookViewer.tsx
-import React, { useState, useEffect } from 'react';
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 const fetchFlipbook = async (pdfLink: string, apiKey: string) => {
-  const url = `https://heyzine.com/api1/rest?pdf=${encodeURIComponent(pdfLink)}&k=${apiKey}`;
+  const url = `https://heyzine.com/api1/rest?pdf=${encodeURIComponent(
+    pdfLink,
+  )}&k=${apiKey}`;
 
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
 
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Error fetching flipbook:', error);
+    console.error("Error fetching flipbook:", error);
     throw error;
   }
 };
 
 const FlipbookViewer = () => {
   const [flipbookData, setFlipbookData] = useState(null);
-  const pdfLink = 'https://testformyjournal.s3.us-east-2.amazonaws.com/FullPlanner.pdf';
-  const apiKey = 'd17fbe1594e35101';
+  const pdfLink =
+    "https://testformyjournal.s3.us-east-2.amazonaws.com/FullPlanner.pdf";
+  const apiKey = "d17fbe1594e35101";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,7 +33,7 @@ const FlipbookViewer = () => {
         const data = await fetchFlipbook(pdfLink, apiKey);
         setFlipbookData(data);
       } catch (error) {
-        console.error('Error fetching flipbook data:', error);
+        console.error("Error fetching flipbook data:", error);
       }
     };
 
@@ -43,7 +46,7 @@ const FlipbookViewer = () => {
         // Using Tailwind CSS to make iframe fullscreen
         <iframe
           src={flipbookData.url}
-          className="w-full h-full border-none"
+          className="h-full w-full border-none"
           title="Flipbook"
         ></iframe>
       ) : (
