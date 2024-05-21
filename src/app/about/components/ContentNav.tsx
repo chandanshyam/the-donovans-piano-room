14b40f0b884dd753cdefce413125789b1cb433e0
aouiniamine <amine.m.aouini@hotmail.com>
2024-05-21 14:03:52 +0100

    last touches for about content nav

--- src/app/about/components/ContentNav.tsx
+++ src/app/about/components/ContentNav.tsx
@@ -10,13 +10,13 @@ export const pages = {
 } 
 export default function ContentNav({page}: {page: string}) {
   return (
-    <div className='flex w-9/12 gap-px h-12'>
+    <div className='flex gap-px h-12' style={{width: '70%'}}>
         <Link 
-            className={'flex-1 flex justify-center items-center '} style={{backgroundColor: page !== pages.whyChooseUs ? '#FFC89599': '#D8BCFD'}}
+            className='flex-1 flex justify-center items-center rounded-tl' style={{backgroundColor: page !== pages.whyChooseUs ? '#FFC89599': '#D8BCFD'}}
             href="/about/why-choose-us"><p>Why Choose Us</p></Link>
-        <Link className={'flex-1 flex justify-center items-center'} style={{backgroundColor: page !== pages.whoWeServe ? '#FFC89599': '#D8BCFD'}} href="/about/who-we-serve"><p>Who We Serve</p></Link>
-        <Link className={'flex-1 flex justify-center items-center'} style={{backgroundColor: page !== pages.membership ? '#FFC89599': '#D8BCFD'}} href="/about/membership"><p>Membership</p></Link>
-        <Link className={'flex-1 flex justify-center items-center'} style={{backgroundColor: page !== pages.FAQs ? '#FFC89599': '#D8BCFD'}} href="/about/FAQs"><p>FAQs</p></Link>
+        <Link className='flex-1 flex justify-center items-center' style={{backgroundColor: page !== pages.whoWeServe ? '#FFC89599': '#D8BCFD'}} href="/about/who-we-serve"><p>Who We Serve</p></Link>
+        <Link className='flex-1 flex justify-center items-center' style={{backgroundColor: page !== pages.membership ? '#FFC89599': '#D8BCFD'}} href="/about/membership"><p>Membership</p></Link>
+        <Link className='flex-1 flex justify-center items-center rounded-tr' style={{backgroundColor: page !== pages.FAQs ? '#FFC89599': '#D8BCFD'}} href="/about/FAQs"><p>FAQs</p></Link>
     </div>
   )
 }
