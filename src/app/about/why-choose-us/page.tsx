e994d7e73ebeb9b2d2d6cb1eb035d2ba7bf54a6f
aouiniamine <amine.m.aouini@hotmail.com>
2024-05-21 21:20:50 +0100

    setup highlighed nav style

--- src/app/about/why-choose-us/page.tsx
+++ src/app/about/why-choose-us/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ContentWrapper from '../components/ContentWrapper'
 import { pages } from '../components/ContentNav'
-import Navbar1 from '@/components/navbars/navbar-1'
+import Navbar1, { pages as navbarPages} from '@/components/navbars/navbar-1'
 
 export default function Page() {
   return (
     <>
-    <Navbar1 />
+    <Navbar1 page={navbarPages.about} />
     <ContentWrapper page={pages.whyChooseUs}>
       <></>
     </ContentWrapper>
--- src/components/navbars/navbar-1.tsx
+++ src/components/navbars/navbar-1.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const pages = {
+export const pages = {
     home: "home",
     about: "about",
     games: "games",
@@ -10,18 +10,19 @@ const pages = {
     contact: "contact",
 }
 export default function Navbar1({page}: {page: string}) {
+    const highlightLink = {color: "#DA6A1C", borderBottomColor: "#DA6A1C", borderBottomWidth: "3px", borderBottomRightRadius: "5px", borderBottomLeftRadius: "5px"}
   return (
-    <nav className='fixed top-0 w-full z-40 bg-white'>
+    <nav className='fixed top-0 w-full z-40 bg-white b'>
         <div className='fixed bg-secondary-purple pl-40 pr-4 rounded-r-full'>
             <Image src="/navbar/Logo.svg" width={225} height={32} alt='The Donovan&apos;s Piano Room' />
         </div>
         <div className='flex float-right h-24 p-y-50 mr-36 gap-16'>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="/">HOME</Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="/about/why-choose-us">ABOUT</Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">GAMES</Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">BOOKSTORE</Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">CONTACT</Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">GAMES</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' style={pages.home === page ? highlightLink: {}} href="/">HOME</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' style={pages.about === page ? highlightLink : {}} href="/about/why-choose-us">ABOUT</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' style={pages.games === page ? highlightLink: {}} href="#">GAMES</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' style={pages.bookstore === page ? highlightLink: {}} href="#">BOOKSTORE</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' style={pages.contact === page ? highlightLink: {}} href="#">CONTACT</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' style={pages.games === page ? highlightLink: {}} href="#">GAMES</Link>
             <Link className='text-primary-purple text-xl font-bold flex items-center' href="#"><Image src="/navbar/Cart.svg" width={20} height={20} alt='Cart'/></Link>
             <Link className='text-white bg-primary-purple rounded-l-full rounded-r-full px-20 h-16 mt-4 flex items-center text-xl font-bold' href="/register">Log in or register</Link>
         </div>
