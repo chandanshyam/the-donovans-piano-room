b3ca4373832e5dbfa5e84d8f2da6ae4a9f3cdcc9
aouiniamine <amine.m.aouini@hotmail.com>
2024-05-21 20:17:03 +0100

    finished navbar with style

--- src/components/navbars/navbar-1.tsx
+++ src/components/navbars/navbar-1.tsx
@@ -11,19 +11,19 @@ const pages = {
 }
 export default function Navbar1({page}: {page: string}) {
   return (
-    <nav className='fixed top-0 w-full z-40'>
+    <nav className='fixed top-0 w-full z-40 bg-white'>
         <div className='fixed bg-secondary-purple pl-40 pr-4 rounded-r-full'>
             <Image src="/navbar/Logo.svg" width={225} height={32} alt='The Donovan&apos;s Piano Room' />
         </div>
-        <div className='flex float-right h-24 p-y-50'>
-            <Link className='text-primary-purple' href="/">HOME</Link>
-            <Link className='text-primary-purple' href="/about/why-choose-us">ABOUT</Link>
-            <Link className='text-primary-purple' href="#">GAMES</Link>
-            <Link className='text-primary-purple' href="#">BOOKSTORE</Link>
-            <Link className='text-primary-purple' href="#">CONTACT</Link>
-            <Link className='text-primary-purple' href="#">GAMES</Link>
-            <Link className='text-primary-purple' href="#"><Image src="/navbar/Cart.svg" width={20} height={20} alt='Cart'/></Link>
-            <Link className='text-white bg-primary-purple' href="/register">Log in or register</Link>
+        <div className='flex float-right h-24 p-y-50 mr-36 gap-16'>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' href="/">HOME</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' href="/about/why-choose-us">ABOUT</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">GAMES</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">BOOKSTORE</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">CONTACT</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#">GAMES</Link>
+            <Link className='text-primary-purple text-xl font-bold flex items-center mt-3' href="#"><Image src="/navbar/Cart.svg" width={20} height={20} alt='Cart'/></Link>
+            <Link className='text-white bg-primary-purple rounded-l-full rounded-r-full px-20 h-16 mt-5 flex items-center text-xl font-bold' href="/register">Log in or register</Link>
         </div>
 
     </nav>
