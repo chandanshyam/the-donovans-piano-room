902075111cdf3906fe7a582c0b1c6395f188dabc
aouiniamine <amine.m.aouini@hotmail.com>
2024-05-21 13:55:55 +0100

    setup dynamic background style

---
+++ src/app/about/components/ContentNav.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const pages = {
+    whyChooseUs: "why-choose-us",
+    whoWeServe: "who-we-serve",
+    membership: "membership",
+    scholarship: "scholarship",
+    FAQs: "faqs" 
+} 
+export default function ContentNav({page}: {page: string}) {
+  return (
+    <div className='flex w-9/12 gap-px h-12'>
+        <Link 
+            className={'flex-1 flex justify-center items-center '} style={{backgroundColor: page !== pages.whyChooseUs ? '#FFC89599': '#D8BCFD'}}
+            href="/about/why-choose-us"><p>Why Choose Us</p></Link>
+        <Link className={'flex-1 flex justify-center items-center'} style={{backgroundColor: page !== pages.whoWeServe ? '#FFC89599': '#D8BCFD'}} href="/about/who-we-serve"><p>Who We Serve</p></Link>
+        <Link className={'flex-1 flex justify-center items-center'} style={{backgroundColor: page !== pages.membership ? '#FFC89599': '#D8BCFD'}} href="/about/membership"><p>Membership</p></Link>
+        <Link className={'flex-1 flex justify-center items-center'} style={{backgroundColor: page !== pages.FAQs ? '#FFC89599': '#D8BCFD'}} href="/about/FAQs"><p>FAQs</p></Link>
+    </div>
+  )
+}
