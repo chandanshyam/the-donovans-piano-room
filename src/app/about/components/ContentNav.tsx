8ee788f625850042ad9a2ee6d524ca6b84a45302
aouiniamine <amine.m.aouini@hotmail.com>
2024-05-21 15:03:24 +0100

    font style

--- src/app/about/components/ContentNav.tsx
+++ src/app/about/components/ContentNav.tsx
@@ -12,11 +12,11 @@ export default function ContentNav({page}: {page: string}) {
   return (
     <div className='flex gap-px h-12' style={{width: '70%'}}>
         <Link 
-            className='flex-1 flex justify-center items-center rounded-tl' style={{backgroundColor: page !== pages.whyChooseUs ? '#FFC89599': '#D8BCFD'}}
-            href="/about/why-choose-us"><p>Why Choose Us</p></Link>
-        <Link className='flex-1 flex justify-center items-center' style={{backgroundColor: page !== pages.whoWeServe ? '#FFC89599': '#D8BCFD'}} href="/about/who-we-serve"><p>Who We Serve</p></Link>
-        <Link className='flex-1 flex justify-center items-center' style={{backgroundColor: page !== pages.membership ? '#FFC89599': '#D8BCFD'}} href="/about/membership"><p>Membership</p></Link>
-        <Link className='flex-1 flex justify-center items-center rounded-tr' style={{backgroundColor: page !== pages.FAQs ? '#FFC89599': '#D8BCFD'}} href="/about/FAQs"><p>FAQs</p></Link>
+            className='flex-1 flex justify-center items-center p-8 rounded-tl' style={{fontSize: "12px", backgroundColor: page !== pages.whyChooseUs ? '#FFEBD5': '#D8BCFD'}}
+            href="/about/why-choose-us"><p className='text-primary-brown font-bold' style={{}}>Why Choose Us</p></Link>
+        <Link className='flex-1 flex justify-center items-center p-8' style={{fontSize: "12px", backgroundColor: page !== pages.whoWeServe ? '#FFEBD5': '#D8BCFD'}} href="/about/who-we-serve"><p style={{}} className='text-primary-brown font-bold'>Who We Serve</p></Link>
+        <Link className='flex-1 flex justify-center items-center p-8' style={{fontSize: "12px", backgroundColor: page !== pages.membership ? '#FFEBD5': '#D8BCFD'}} href="/about/membership"><p style={{}} className='text-primary-brown font-bold'>Membership</p></Link>
+        <Link className='flex-1 flex justify-center items-center p-8 rounded-tr' style={{fontSize: "12px", backgroundColor: page !== pages.FAQs ? '#FFEBD5': '#D8BCFD'}} href="/about/FAQs"><p  style={{}}className='text-primary-brown font-bold'>FAQs</p></Link>
     </div>
   )
 }
