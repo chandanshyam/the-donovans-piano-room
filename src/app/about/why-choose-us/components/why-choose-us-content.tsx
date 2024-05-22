eededc8fac7103cd4e44e7b92c0de73177b986c2
aouiniamine <amine.m.aouini@hotmail.com>
2024-05-22 13:36:07 +0100

    add second row icons & fix style

--- src/app/about/why-choose-us/components/why-choose-us-content.tsx
+++ src/app/about/why-choose-us/components/why-choose-us-content.tsx
@@ -15,18 +15,33 @@ export default function WhyChooseUsContent() {
         </div>
       </div>
     </div>
-    <div className='flex-1 grid grid-cols-3 justify-center items-center'>
-      <div className='w-52 h-60 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col items-center justify-center gap-3 shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
-        <Image src="/about/why-choose-us/Icon1.svg" alt="" width={68} height={56} />
-        <p className='text-[#696061] text-3xl text-center font-medium'>Note Recognition</p>
-      </div>
-      <div className='w-52 h-60 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col items-center justify-center gap-3 shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
-        <Image src="/about/why-choose-us/Icon2.svg" alt="" width={68} height={56} />
-        <p className='text-[#696061] text-3xl text-center font-medium'>Chord Recognition</p>
-      </div>
-      <div className='w-52 h-60 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col items-center justify-center gap-3 shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
-        <Image src="/about/why-choose-us/Icon3.svg" alt="" width={68} height={56} />
-        <p className='text-[#696061] text-3xl text-center font-medium w-28'>Ear Training</p>
+    <div className='flex-1 flex items-center justify-start'>
+      <div className='grid grid-cols-3 grid-rows-2 justify-center items-center h-6/12 gap-y-16 gap-x-7'>
+
+        <div className='w-60 h-64 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col gap-3 items-center justify-center shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
+          <Image src="/about/why-choose-us/Icon1.svg" alt="" width={68} height={56} />
+          <p className='text-[#696061] text-3xl text-center font-medium'>Note Recognition</p>
+        </div>
+        <div className='w-60 h-64 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col gap-3 items-center justify-center shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
+          <Image src="/about/why-choose-us/Icon2.svg" alt="" width={68} height={56} />
+          <p className='text-[#696061] text-3xl text-center font-medium'>Chord Recognition</p>
+        </div>
+        <div className='w-60 h-64 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col gap-3 items-center justify-center shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
+          <Image src="/about/why-choose-us/Icon3.svg" alt="" width={68} height={56} />
+          <p className='text-[#696061] text-3xl text-center font-medium w-28'>Ear Training</p>
+        </div>
+        <div className='w-60 h-64 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col gap-3 items-center justify-center shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
+          <Image src="/about/why-choose-us/Icon4.svg" alt="" width={68} height={56} />
+          <p className='text-[#696061] text-3xl text-center font-medium'>Rhythm</p>
+        </div>
+        <div className='w-60 h-64 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col gap-3 items-center justify-center shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
+          <Image src="/about/why-choose-us/Icon5.svg" alt="" width={68} height={56} />
+          <p className='text-[#696061] text-3xl text-center font-medium'>Intervals</p>
+        </div>
+        <div className='w-60 h-64 bg-white border-2 border-[#FCF0D8] rounded-3xl flex flex-col gap-3 items-center justify-center shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px]'>
+          <Image src="/about/why-choose-us/Icon6.svg" alt="" width={68} height={56} />
+          <p className='text-[#696061] text-3xl text-center font-medium'>Music Vocabulary</p>
+        </div>
       </div>
     </div>
   </div>
