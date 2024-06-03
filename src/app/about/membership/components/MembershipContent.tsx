03165184bd67f4f0d55fbaf5ec5a1104ee44dc4f
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-03 21:07:59 +0100

    finished membership content

--- src/app/about/membership/components/MembershipContent.tsx
+++ src/app/about/membership/components/MembershipContent.tsx
@@ -29,6 +29,50 @@ export default function MembershipContent() {
                 </ul>
                 <Link className='w-[90%] mt-[5vh] self-center text-white bg-primary-purple rounded-l-full rounded-r-full px-20 py-3 flex items-center justify-center text-xl font-bold' href=''>Choose plan</Link>
             </div>
+            <div className='flex flex-col bg-white border-2 border-[#FCF0D8] rounded-3xl shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px] w-[22%] h-[32vh] p-6'>
+                <h4 className='text-[20px]'>Monthly Access</h4>
+                <ul className='mt-6'>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Introduction</p>
+                    </li>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Access to all games and lessons</p>
+                    </li>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Progress tracking</p>
+                    </li>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-doesnt-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Bundled savings</p>
+                    </li>
+                </ul>
+                <Link className='w-[90%] mt-[5vh] self-center text-white bg-primary-purple rounded-l-full rounded-r-full px-20 py-3 flex items-center justify-center text-xl font-bold' href=''>Choose plan</Link>
+            </div>
+            <div className='flex flex-col bg-white border-2 border-[#FCF0D8] rounded-3xl shadow-[#AC7A2280] shadow-[rgba(0,0,15,0.5)_2px_3px_4px_0px] w-[22%] h-[32vh] p-6'>
+                <h4 className='text-[20px]'>Yearly Access</h4>
+                <ul className='mt-6'>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Introduction</p>
+                    </li>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Access to all games and lessons</p>
+                    </li>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Progress tracking</p>
+                    </li>
+                    <li className='flex gap-6 mt-2'>
+                        <Image src='/about/membership/Icon-include.svg' alt='include' width={15} height={15}></Image>
+                        <p className='text-[12px]'>Bundled savings</p>
+                    </li>
+                </ul>
+                <Link className='w-[90%] mt-[5vh] self-center text-white bg-primary-purple rounded-l-full rounded-r-full px-20 py-3 flex items-center justify-center text-xl font-bold' href=''>Choose plan</Link>
+            </div>
         </div>
     </div>
   )
