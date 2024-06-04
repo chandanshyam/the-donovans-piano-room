c03e6c748edcb6ed585351e9c3974d35992b1578
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-04 15:36:51 +0100

    fix rounded scholarship nav component

--- src/app/about/scholarship/components/AboutMembershipNav.tsx
+++ src/app/about/scholarship/components/AboutMembershipNav.tsx
@@ -6,9 +6,9 @@ export default function AboutMembershipNav({page}: {page: string}) {
   return (
     <div className='flex gap-px h-12' style={{width: '45%'}}>
         <Link 
-            className='flex-1 flex justify-center items-center p-8 text-[12px] laptop:text-[16px] desktop:py-10 desktop:text-[18px] rounded-tl-xl' style={{backgroundColor: page !== aboutScholarshipNavigation.whatsIncluded ? '#FFEBD5': '#D8BCFD'}}
+            className='flex-1 flex justify-center items-center p-8 text-[12px] laptop:text-[16px] desktop:py-10 desktop:text-[18px] rounded-l-xl' style={{backgroundColor: page !== aboutScholarshipNavigation.whatsIncluded ? '#FFEBD5': '#D8BCFD'}}
             href="/about/scholarship/what-is-included"><p className='text-primary-brown font-bold' style={{}}>What’s included in a scholarship</p></Link>
-        <Link className='flex-1 flex justify-center items-center p-8 text-[12px] laptop:text-[16px] desktop:py-10 desktop:text-[18px] rounded-tr-xl' style={{backgroundColor: page !== aboutScholarshipNavigation.whosEligible ? '#FFEBD5': '#D8BCFD'}} href="#"><p  style={{}}className='text-primary-brown font-bold'>Who is eligible for a scholarship</p></Link>
+        <Link className='flex-1 flex justify-center items-center p-8 text-[12px] laptop:text-[16px] desktop:py-10 desktop:text-[18px] rounded-r-xl' style={{backgroundColor: page !== aboutScholarshipNavigation.whosEligible ? '#FFEBD5': '#D8BCFD'}} href="#"><p  style={{}}className='text-primary-brown font-bold'>Who is eligible for a scholarship</p></Link>
     </div>
   )
 }
