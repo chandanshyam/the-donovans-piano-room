05a4f2ffc3b730d43276e9a668f6dfc484bf2bb2
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-06 23:18:24 +0100

    fix register nav button & fix z index

--- src/components/navbars/Navbar2.tsx
+++ src/components/navbars/Navbar2.tsx
@@ -4,11 +4,9 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar2(){
-    // const highlightLink = {color: "#DA6A1C"}
-    // const displayBorder = () => <div className='w-full bg-tertiary-orange absolute bottom-0 rounded-tl-xl rounded-tr-xl' style={{height: "3px"}}></div>
   return (
-    <nav className='fixed top-0 w-full z-40 border-b-2 border-[#A135E8] py-2'>
-        <div className='fixed bg-[#601D86] pl-40 pr-4 py-2 rounded-r-full top-0 '>
+    <nav className='fixed top-0 w-full z-40 border-b-2 border-[#A135E8] py-2 z-50'>
+        <div className='fixed bg-[#601D86] pl-40 pr-4 py-2 rounded-r-full top-0'>
             <Image src="/navbar/Logo2.svg" width={220} height={35} alt='The Donovan&apos;s Piano Room' />
         </div>
         <div className='flex float-right h-24 p-y-50 mr-36 gap-16'>
@@ -28,7 +26,7 @@ export default function Navbar2(){
              <p>CONTACT</p>
             </Link>
             <Link className='text-primary-yellow text-xl font-bold flex items-center' href="#"><Image src="/navbar/CartYellow.svg" width={40} height={40} alt='Cart'/></Link>
-            <Link className='text-primary-purple bg-primary-yellow rounded-l-full rounded-r-full px-20 h-16 mt-4 flex items-center text-xl font-bold' href="/register">Log in or register</Link>
+            <Link className='text-primary-purple bg-primary-yellow rounded-l-full rounded-r-full px-20 h-16 mt-4 flex items-center text-xl font-bold' href="/signup">Log in or register</Link>
         </div>
 
     </nav>
