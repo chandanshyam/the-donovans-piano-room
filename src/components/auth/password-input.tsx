5d5a6be5a3efc0f9ea39b4309157f271fd577a31
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-07 22:53:12 +0100

    setup password input

---
+++ src/components/auth/password-input.tsx
@@ -0,0 +1,36 @@
+import { FormControl, IconButton, Input, InputAdornment, InputLabel } from '@mui/material'
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import React from 'react'
+interface passwordInputInterface {
+    name: string,
+    showPassword: boolean,
+    onChange: any,
+    toggleShowPassword: any,
+    // error: string
+}
+export default function PasswordInput({ onChange, showPassword, name, toggleShowPassword}: passwordInputInterface) {
+  return (
+    <FormControl variant="filled" sx={{border: 0}} className='focus:bg-white block rounded-3xl bg-[#E3E3E4]'>
+        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+        <Input
+            required
+            sx={{border: 0}}
+            className='w-full py-1 pl-6 text-[16px]'
+            type={showPassword ? 'text' : 'password'}
+            name={name}
+            onChange={onChange}
+            endAdornment={
+            <InputAdornment position="end">
+            <IconButton
+                aria-label="toggle password visibility"
+                onClick={toggleShowPassword}>
+                {showPassword ? <VisibilityOff className='mb-5'/> : <Visibility className='mb-5'/>}
+            </IconButton>
+            </InputAdornment>
+        }
+        />
+    </FormControl>
+
+  )
+}
