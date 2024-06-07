cc59117009a8e071bc1dbfa7056d07eb59e8cbf4
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-07 22:51:52 +0100

    rm old form input

--- src/components/auth/form-input.tsx
+++ src/components/auth/form-input.tsx
@@ -1,37 +1,36 @@
+import { FormControl, Input, InputLabel } from '@mui/material'
 export interface InputData {
-  id: string;
   name: string;
-  label: string;
   type: string;
-  placeholder: string;
+  label: string;
 }
 
-export function Input({
+export default function InputForm({
   field,
-  register,
-  errors,
+  error,
+  text,
+  onChange,
 }: {
   field: InputData;
-  register: any;
-  errors: any;
+  error: string;
+  text: string;
+  onChange: any;
 }) {
   return (
     <div>
-      <label
-        htmlFor={field.id}
-        className="mb-2 block text-sm font-medium text-gray-900"
-      >
-        {field.label}
-      </label>
-      <input
-        className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
-        type={field.type}
-        placeholder={field.placeholder}
-        {...register(field.name)}
-      />
-      {errors[field.name]?.message && (
-        <p className="text-sm text-red-400">{errors[field.name].message}</p>
-      )}
+      <FormControl variant="filled" className='focus:bg-white block rounded-3xl bg-[#E3E3E4]' error={!!error}>
+        <InputLabel htmlFor="standard-adornment-password">{field.label}</InputLabel>
+        <Input
+          required
+          className='w-full pl-6 text-[16px]'
+          type={field.type}
+          value={text}
+          name={field.name}
+          onChange={onChange}
+          error={!!error}
+        />
+      </FormControl>
+      {error && (<p className='mt-3'>{error}</p>)}
     </div>
   );
 }
