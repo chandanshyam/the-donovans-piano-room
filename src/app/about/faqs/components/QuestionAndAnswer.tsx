b2812d34f0ab147d25b4b64280b2902fc23b3385
Manas Paranjape <mparanja@purdue.edu>
2024-06-08 20:49:22 -0400

    modified answers to be react components instead of strings

--- src/app/about/faqs/components/QuestionAndAnswer.tsx
+++ src/app/about/faqs/components/QuestionAndAnswer.tsx
@@ -1,26 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function QuestionAndAnswer({question, answer}: {question: string, answer: string}) {
+export default function QuestionAndAnswer({question, answer}: {question: string, answer: React.ReactNode}) {
   return (
     <details className="group mb-7 w-[75%]">
-
-            <summary
-                className="flex items-center justify-between gap-2 p-2 font-medium marker:content-none hover:cursor-pointer">
-
-                <span className="flex justify-between">
-                    <span className='text-primary-purple text-[18px]'>
-                        {question}
-                    </span>
+        <summary
+            className="flex items-center justify-between gap-2 p-2 font-medium marker:content-none hover:cursor-pointer">
+            <span className="flex justify-between">
+                <span className='text-primary-purple text-[18px]'>
+                    {question}
                 </span>
-                <Image className="w-5 h-5 text-gray-500 transition group-open:rotate-0 rotate-180" src="/about/FAQs/DropdownIcon.svg"
-                    width={18} height={18} alt=''/>
-            </summary>
+            </span>
+            <Image className="w-5 h-5 text-gray-500 transition group-open:rotate-0 rotate-180" src="/about/FAQs/DropdownIcon.svg" width={18} height={18} alt=''/>
+        </summary>
 
-            <article className="px-2 pb-4">
-                <p className='text-[15px]'>{answer}</p>
-            </article>
+        <article className="px-2 pb-4">
+            <div className='text-[15px]'>{answer}</div>
+        </article>
 
-        </details>
+    </details>
   )
 }
