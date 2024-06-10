72e4bee36933410518c8b2673e08163a0f8128c8
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-10 15:47:03 +0100

    paasword input finished

--- src/components/auth/password-input.tsx
+++ src/components/auth/password-input.tsx
@@ -1,37 +1,40 @@
 import { FormControl, IconButton, Input, InputAdornment, InputLabel } from '@mui/material'
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import React from 'react'
+import React, { useState } from 'react'
 interface passwordInputInterface {
     name: string,
-    showPassword: boolean,
     onChange: any,
-    toggleShowPassword: any,
     // error: string
 }
-export default function PasswordInput({ onChange, showPassword, name, toggleShowPassword}: passwordInputInterface) {
+export default function PasswordInput({ onChange, name}: passwordInputInterface) {
+  const [showPassword, setShowPassword] = useState(false)
+  const toggleShowPassword = () => setShowPassword(prev => !prev)
   return (
-    <FormControl variant="filled" sx={{border: 0}} className='focus:bg-white block rounded-3xl bg-[#E3E3E4] w-full'>
-        <InputLabel sx={[{color: "#391f0f"},()=>({'&.Mui-focused': {color: "#391f0f"}})]} htmlFor="standard-adornment-password">Password</InputLabel>
-        <Input
-            required
-            sx={{border: 0}}
-            className='w-full py-1 pl-6 text-[16px]'
-            type={showPassword ? 'text' : 'password'}
-            name={name}
-            onChange={onChange}
-            disableUnderline
-            endAdornment={
-            <InputAdornment position="end">
-            <IconButton
-                aria-label="toggle password visibility"
-                onClick={toggleShowPassword}>
-                {showPassword ? <VisibilityOff className='mb-6 mr-2'/> : <Visibility className='mb-6 mr-2'/>}
-            </IconButton>
-            </InputAdornment>
-        }
-        />
-    </FormControl>
+    <div>
+
+      <FormControl variant="filled" sx={{border: 0}} className='focus:bg-white block rounded-3xl bg-[#E3E3E4] w-full 2xl:mb-[25px] 2xl:py-2'>
+          <InputLabel sx={[{color: "#391f0f"},()=>({'&.Mui-focused': {color: "#391f0f"}})]} htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+              required
+              sx={{border: 0}}
+              className='w-full py-1 pl-6 text-[16px]'
+              type={showPassword ? 'text' : 'password'}
+              name={name}
+              onChange={onChange}
+              disableUnderline
+              endAdornment={
+                <InputAdornment position="end">
+              <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={toggleShowPassword}>
+                  {showPassword ? <Visibility className='mb-6 mr-2'/> : <VisibilityOff className='mb-6 mr-2'/>}
+              </IconButton>
+              </InputAdornment>
+          }
+          />
+      </FormControl>
+  </div>
 
   )
 }
