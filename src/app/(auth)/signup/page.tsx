22de4b944f6f0c3a32303c2fcefa9fae28ba2832
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-11 20:42:57 +0100

    fix radio color

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import SignupContentWrapper from "@/components/auth/signup/SignupContentWrapper";
 import SignupForm from "./components/SignupForm";
+import MembershipSelctionLayout from "./components/membership/MembershipSelctionLayout";
 export default function SignUp() {
 
   return (
     <SignupContentWrapper>
-      <SignupForm/>
-      
+      {/* <SignupForm/> */}
+      <MembershipSelctionLayout/>
     </SignupContentWrapper>
     
   );
