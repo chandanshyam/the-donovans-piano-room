cf5a83beaec0c2888034716c747947c8232323bc
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-12 20:31:22 +0100

    fix

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,13 +2,19 @@
 import SignupContentWrapper from "@/components/auth/signup/SignupContentWrapper";
 // import SignupForm from "./components/SignupForm";
 import SignupMembershipContent from "./components/membership/SignupMembershipContent";
+import { Providers } from "@/app/providers";
 export default function SignUp() {
 
   return (
-    <SignupContentWrapper>
-      {/* <SignupForm/> */}
-      <SignupMembershipContent />
-    </SignupContentWrapper>
+    <>
+      <Providers>
+
+        <SignupContentWrapper>
+          {/* <SignupForm/> */}
+          <SignupMembershipContent />
+        </SignupContentWrapper>
+      </Providers>
+    </>
     
   );
 }
