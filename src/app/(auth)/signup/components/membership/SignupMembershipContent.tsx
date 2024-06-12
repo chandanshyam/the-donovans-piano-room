2e53569b48ad4985750480edab1a265493e61820
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-12 20:31:22 +0100

    fix

--- src/app/(auth)/signup/components/membership/SignupMembershipContent.tsx
+++ src/app/(auth)/signup/components/membership/SignupMembershipContent.tsx
@@ -1,13 +1,12 @@
-import { Provider } from "jotai";
 import MembershipIncludes from "./MembershipIncludes";
 import MembershipSelctionLayout from "./MembershipSelctionLayout";
 
 export default function SignupMembershipContent() {
   return (
-    <Provider>
+    <>
         <MembershipSelctionLayout />
         <MembershipIncludes />
-    </Provider>
+    </>
   )
 }
 
