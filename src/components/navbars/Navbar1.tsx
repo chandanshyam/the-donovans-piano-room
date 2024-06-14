e77f81fa2c6037116be4c4f3d1ab88ca7679670b
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-14 01:10:27 +0100

    update nav1 changes

--- src/components/navbars/Navbar1.tsx
+++ src/components/navbars/Navbar1.tsx
@@ -1,41 +1,45 @@
 import { navigationPages } from '@/utils/general'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 export default function Navbar1({page}: {page: string}) {
     const highlightLink = {color: "#DA6A1C"}
-    const displayBorder = () => <div className='w-full bg-tertiary-orange absolute bottom-0 rounded-tl-xl rounded-tr-xl' style={{height: "3px"}}></div>
+    const displayBorder = () => <div className='w-full bg-tertiary-orange absolute bottom-0 rounded-tl-xl rounded-tr-xl h-[3px] 2xl:h-[5px] xl:h-[4px]'></div>
   return (
-    <nav className='fixed top-0 w-full z-40 bg-white z-50'>
-        <div className='fixed bg-secondary-purple pl-40 pr-4 rounded-r-full'>
-            <Image src="/navbar/Logo.svg" width={225} height={32} alt='The Donovan&apos;s Piano Room' />
+    <>
+    
+    <div className='fixed h-[8.8vh] w-[100vw] backdrop-blur-sm z-40 bg-[#fbf7ff]'></div>
+    <nav className='fixed top-0 w-full bg-white z-50 h-[9vh] border-b-2 border-b-[#ecd6fe]'>
+        <div className='absolute bg-secondary-purple h-[8.8vh] w-[24vw] pr-4 py-2 rounded-r-full top-0'>
+            <Image src="/navbar/Logo.svg" fill alt='The Donovan&apos;s Piano Room' />
         </div>
-        <div className='flex float-right h-24 p-y-50 mr-36 gap-16'>
-            <Link className='text-primary-purple text-xl font-bold flex items-center relative'style={navigationPages.home === page ? highlightLink: {}} href="/">
+
+        <div className='absolute flex float-right h-[8.8vh] p-y-50 top-[0px] right-36 gap-16 justify-center h-full'>
+            <Link className='text-primary-purple text-xl 2xl:text-3xl font-bold flex items-center relative'style={navigationPages.home === page ? highlightLink: {}} href="/">
              <p>HOME</p>
              {navigationPages.home === page && displayBorder()}
             </Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center relative' style={navigationPages.about === page ? highlightLink : {}} href="/about/why-choose-us">
+            <Link className='text-primary-purple text-xl 2xl:text-3xl font-bold flex items-center relative' style={navigationPages.about === page ? highlightLink : {}} href="/about/why-choose-us">
               <p>ABOUT</p>
               {navigationPages.about === page && displayBorder()}
             </Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center relative' style={navigationPages.games === page ? highlightLink: {}} href="#">
+            <Link className='text-primary-purple text-xl 2xl:text-3xl font-bold flex items-center relative' style={navigationPages.games === page ? highlightLink: {}} href="#">
               <p>GAMES</p>
               {navigationPages.games === page && displayBorder()}
             </Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center relative' style={navigationPages.bookstore === page ? highlightLink: {}} href="#">
+            <Link className='text-primary-purple text-xl 2xl:text-3xl font-bold flex items-center relative' style={navigationPages.bookstore === page ? highlightLink: {}} href="#">
              <p>BOOKSTORE</p>
              {navigationPages.bookstore === page && displayBorder()}
             </Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center relative' style={navigationPages.contact === page ? highlightLink: {}} href="#">
+            <Link className='text-primary-purple text-xl 2xl:text-3xl font-bold flex items-center relative' style={navigationPages.contact === page ? highlightLink: {}} href="#">
              <p>CONTACT</p>
              {navigationPages.contact === page && displayBorder()}
             </Link>
-            <Link className='text-primary-purple text-xl font-bold flex items-center' href="#"><Image src="/navbar/Cart.svg" width={20} height={20} alt='Cart'/></Link>
-            <Link className='text-white bg-primary-purple rounded-l-full rounded-r-full px-20 h-16 mt-4 flex items-center text-xl font-bold' href="/signup">Log in or register</Link>
+            <Link className='flex items-center' href="#"><Image src="/navbar/Cart.svg" width={20} height={20} alt='Cart'/></Link>
+            <Link className='text-white bg-primary-purple rounded-l-full rounded-r-full px-20 h-16 flex items-center text-xl 2xl:text-3xl font-bold self-center' href="/signup">Log in or register</Link>
         </div>
 
     </nav>
+    </>
   )
 }
