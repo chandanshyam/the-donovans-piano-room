bb3b0170bcfbdbea61233669e6d1c96cd24e81f5
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-14 01:10:12 +0100

    update footer 2 changes

--- src/components/footers/Footer2.tsx
+++ src/components/footers/Footer2.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 export default function Footer2() {
   return (
-    <div className='fixed flex justify-center items-center bottom-0 w-full border-t-2 border-[#A135E8]' style={{height: '70px'}}>
-        <div className='flex justify-between' style={{width: '84.7%', height: '28px'}}>
-            <h2 className='text-white'
-            style={{width: '290px'}}
+    <div className='fixed flex justify-center items-center bottom-0 w-full border-t-2 border-[#A135E8] h-[9vh]'>
+        <div className='fixed h-[8.8vh] w-[100vw] backdrop-blur-sm z-40'></div>
+        <div className='flex justify-between h-[28px] w-[84.7%] z-50'>
+            <h2 className='text-white 2xl:text-[14px] w-[290px] 2xl:w-[400px]'
             >Copyright © 2024 The Donovan&apos;s Piano Room. Powered by <Link className='text-primary-yellow underline' target='_blank' href='https://www.thedonovan.org/'>The Donovan&apos;s Venom</Link>, a 501(c)(3) nonprofit organization.</h2>
             <div className='flex gap-8'>
                 <Image src="/footer/YellowTwitter.svg" width={40} height={40} alt='Twitter' />
