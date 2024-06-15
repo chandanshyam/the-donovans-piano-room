b265384bd941e0da154de9c1bc0a44d5a7ee96f8
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-15 14:34:16 +0100

    fix checkbox issues

--- src/app/(auth)/signup/components/SignupForm.tsx
+++ src/app/(auth)/signup/components/SignupForm.tsx
@@ -73,10 +73,10 @@ export default function SignupForm() {
             <span
                 className="absolute text-primary-purple transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"
-                    stroke="currentColor" stroke-width="1">
-                    <path fill-rule="evenodd"
+                    stroke="currentColor" strokeWidth="1">
+                    <path fillRule="evenodd"
                         d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd">
+                        clipRule="evenodd">
                     </path>
                 </svg>
             </span>
