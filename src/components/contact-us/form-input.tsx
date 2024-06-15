66762bb0a7173174b45e6dee477c9222ee02e160
Manas Paranjape <mparanja@purdue.edu>
2024-06-15 16:00:53 -0400

    added form input

---
+++ src/components/contact-us/form-input.tsx
@@ -0,0 +1,41 @@
+import { FormControl, Input, InputLabel } from '@mui/material'
+export interface InputData {
+  name: string;
+  type: string;
+  label: string;
+}
+
+export default function InputForm({
+  field,
+  error,
+  text,
+  onChange,
+  numRows=1,
+}: {
+  field: InputData;
+  error: string;
+  text: string;
+  onChange: any;
+  numRows?: number;
+}) {
+  return (
+    <div>
+      <FormControl variant="filled" className='focus:bg-white block rounded-3xl bg-[#E3E3E4] w-full 2xl:mb-[25px] 2xl:py-2' error={!!error}>
+        <InputLabel sx={[{color: "#391f0f"},()=>({'&.Mui-focused': {color: "#391f0f"}})]} htmlFor="standard-adornment-password">{field.label}</InputLabel>
+        <Input
+          required
+          className='w-full pl-6 text-[16px]'
+          type={field.type}
+          value={text}
+          name={field.name}
+          onChange={onChange}
+          error={!!error}
+          disableUnderline
+          multiline={field.type === 'textarea'}
+          rows={numRows}
+        />
+      </FormControl>
+      {error && (<p className='mt-3'>{error}</p>)}
+    </div>
+  );
+}
