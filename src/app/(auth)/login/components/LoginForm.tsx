40174397fe5d3bb6e9400e16180ae5c7f69edab4
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-19 18:32:47 +0100

    refinements

--- src/app/(auth)/login/components/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm.tsx
@@ -8,7 +8,7 @@ export default function LoginForm() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState('')
     return (
-        <div className="w-[24vw]">
+        <div className="w-[24vw] 3xl:w-[26vw]">
             <Link href="/" className="text-primary-yellow text-xl font-bold flex relative w-[15%] mb-5"><Image src="/YellowBackIcon.svg" width={30} height={30} alt=""/><p className="mt-2">Home</p></Link>
         <h1 className="text-7xl font-bold leading-tight tracking-tight text-white mb-5">
         Log In
@@ -40,11 +40,11 @@ export default function LoginForm() {
                 <div className="flex items-center">
                     <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
                         <input type="checkbox"
-                            className="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border before:border-[#391f0f] checked:border-primary-yellow transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:bg-primary-yellow bg-[#fef8ee] hover:before:opacity-10"
+                            className="before:content[''] peer relative h-6 w-6 4xl:h-8 4xl:w-8 cursor-pointer appearance-none rounded-md border before:border-[#391f0f] checked:border-primary-yellow transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:bg-primary-yellow bg-[#fef8ee] hover:before:opacity-10"
                             id="check" />
                         <span
                             className="absolute text-primary-purple transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 4xl:h-7 4xl:w-7" viewBox="0 0 20 20" fill="currentColor"
                                 stroke="currentColor" strokeWidth="1">
                                 <path fillRule="evenodd"
                                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
@@ -53,15 +53,15 @@ export default function LoginForm() {
                             </svg>
                         </span>
                     </label>
-                    <label className="text-lg font-medium text-white mt-1 2xl:mt-2 2xl:text-xl">Remember me</label>
+                    <label className="text-lg font-medium text-white mt-1 2xl:mt-2 3xl:text-2xl 4xl:text-[16px]">Remember me</label>
                 </div>
-                <Link href="/forgot-password" className="text-lg font-medium text-primary-yellow mt-3 2xl:mt-4 2xl:text-xl">Forgot password?</Link>
+                <Link href="/forgot-password" className="text-lg font-medium text-primary-yellow mt-3 2xl:mt-4 3xl:text-2xl 4xl:text-[16px]">Forgot password?</Link>
             </div>
             <div> 
-                <button className='w-full text-center bg-primary-yellow py-3 rounded-3xl text-[12px] text-primary-purple font-semibold 2xl:py-5 2xl:rounded-full' type='submit'>Continue to verify account</button>
+                <button className='w-full text-center bg-primary-yellow py-3 rounded-3xl text-[12px] 3xl:text-2xl 4xl:text-[16px] text-primary-purple font-semibold 2xl:py-5 2xl:rounded-full 2xl:py-5 3xl:py-7 2xl:rounded-full' type='submit'>Continue to verify account</button>
             </div>
         </form>
-        <p className='w-full text-center mt-[10px] text-lg text-white bg-primary-purple py-3 rounded-3xl text-[12px] mt-9 2xl:py-5 2xl:rounded-full'>Don&apos;t have an account? <Link href="/signup" className='text-primary-yellow underline'>Sign up</Link></p>
+        <p className='w-full text-center mt-[10px] text-lg text-white bg-primary-purple py-3 rounded-3xl text-[12px] mt-9 2xl:py-5 2xl:rounded-full 3xl:text-2xl 4xl:text-[16px] 3xl:py-8'>Don&apos;t have an account? <Link href="/signup" className='text-primary-yellow underline'>Sign up</Link></p>
     
         </div>
     )
