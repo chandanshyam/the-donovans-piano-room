177552220075f514a61dd2cb020162be9f3aa8a7
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-20 17:05:16 +0100

    setup reset password form

---
+++ src/app/(auth)/reset-password/[token]/components/ResetPasswordContent.tsx
@@ -0,0 +1,28 @@
+import AuthOptionalNavigation from "@/components/atoms/AuthOptionalNavigation";
+import Button1 from "@/components/atoms/Button1";
+import PasswordCases from "@/components/auth/PasswordCases";
+import PasswordInput from "@/components/auth/password-input";
+import { useState } from "react";
+
+export default function ResetPasswordContent() {
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [allPasswordCasesCorrect, setAllPasswordCasesCorrect] = useState(false)
+    const handleSubmit = (e: any) =>{
+        e.preventDefault()
+    }
+  return (
+    <section className="w-[24vw] 2xl:w-[26vw]">
+        <h1 className="text-7xl 3xl:text-8xl font-bold leading-tight tracking-tight text-white mb-7">Reset your password</h1>
+        <p className='text-white text-2xl 3xl:text-4xl font-semibold'>Choose a new and secure password to protect your account</p>
+        <form className="flex flex-col gap-8 mt-7 mb-7">
+
+            <PasswordInput inputValue={password} error="" onChange={(e:any) => setPassword(e.target.value)} name="new-password" label="New password" />
+            <PasswordCases  testCasesCB={setAllPasswordCasesCorrect} password={password} allCasesIsCorrect={allPasswordCasesCorrect} />
+            <PasswordInput inputValue={confirmPassword} error="" onChange={(e:any) => setPassword(e.target.value)} name="confirm-password" label="Confirm password" />
+            <Button1 text="Update your password" onClick={handleSubmit} />
+        </form>
+        <AuthOptionalNavigation text="Don't have an account? " href="/signup" navName="Sign up"/>
+    </section>
+  )
+}
