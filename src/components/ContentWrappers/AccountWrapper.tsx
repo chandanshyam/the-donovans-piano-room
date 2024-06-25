bfb6370d405fe049cfbdfb8b24f537fd99048414
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-25 19:47:30 +0100

    header finished

--- src/components/ContentWrappers/AccountWrapper.tsx
+++ src/components/ContentWrappers/AccountWrapper.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer4 from "../footers/Footer4";
 import Navbar4Left from "../navbars/Navbar4Left";
 import Image from "next/image";
+import { profile } from "@/utils/general";
 
 export default function AccountWrapper({children}: {children: React.ReactNode}) {
   return (
@@ -10,7 +11,18 @@ export default function AccountWrapper({children}: {children: React.ReactNode}) 
       <div className="w-full">
 
         <div className="h-[90.5vh]">
-            {children}
+          <div className="relative z-40 h-[9.7vh] border-b border-[#FED2AA] flex justify-center items-center">
+            <div className="w-[84.7%] flex justify-between items-center">
+              <h1 className="text-5xl 3xl:text-6xl 4xl:text-7xl font-montserrat font-medium">Account & Settings</h1>
+              <div className="bg-[#FED2AA] p-5 rounded-full flex items-center gap-[.5vw]">
+                <div className="relative h-[3vh] w-[3vh]">
+                  <Image src={profile.imageSrc} fill alt=""/>
+                </div>
+                <p className="text-2xl 3xl:text-3xl 4xl:text-4xl font-medium">{profile.fullName}</p>
+              </div>
+            </div>
+          </div>
+          {children}
         </div>
         <Footer4 />
       </div>
