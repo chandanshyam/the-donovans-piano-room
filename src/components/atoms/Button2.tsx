8f4367c21ff4d86b4ab203618225e2371be39b42
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-27 20:23:32 +0100

    update buttons 1 and 2 to be reused in forms with on click needed

--- src/components/atoms/Button2.tsx
+++ src/components/atoms/Button2.tsx
@@ -1,4 +1,4 @@
-export default function Button2({text, onClick}: {text: string, onClick: any}) {
+export default function Button2({text, onClick=null}: {text: string, onClick?: any}) {
   return (
     <button onClick={onClick} className="w-full border border-primary-yellow py-3 2xl:py-4 rounded-full text-[12px] 2xl:text-2xl 4xl:text-3xl font-semibold text-primary-yellow">{text}</button>
   )
