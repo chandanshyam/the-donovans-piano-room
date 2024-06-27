5238f0fc151a7072a010a544f42c76a148b28451
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-27 20:27:51 +0100

    fix select input

--- src/components/atoms/select-input.tsx
+++ src/components/atoms/select-input.tsx
@@ -1,17 +1,24 @@
-import { FormControl, InputLabel, MenuItem, NativeSelect, Select } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, MenuList, Select } from '@mui/material'
 import React from 'react'
 
 export default function selectInput(
-    {name, label, value, options,}:
-    {name: string, label: string, value: string, options: string[]}) {
+    {name, label, value, options, onChange}:
+    {name: string, label: string, value: string, options: string[], onChange: any}) {
   return (
     <FormControl fullWidth>
-        <InputLabel variant="filled" className='3xl:text-2xl 3xl:mt-2' sx={[{color: "#391f0f"},()=>({'&.Mui-focused': { color: "#391f0f"}})]} htmlFor="uncontrolled-native">
+        <InputLabel variant="filled" className='3xl:text-2xl' sx={[{color: "#391f0f"},()=>({'&.Mui-focused': { color: "#391f0f"}})]} htmlFor="uncontrolled-native">
             {label}
         </InputLabel>
         <Select
+            onChange={onChange}
             disableUnderline
             value={value}
+            MenuProps={{
+                PaperProps: {
+                    bgcolor: '#FEF8EE',
+                },
+                
+            }}
             inputProps={{
             name,
             id: 'uncontrolled-native',
@@ -19,7 +26,12 @@ export default function selectInput(
             sx={[{ border: 1, '.MuiOutlinedInput-notchedOutline': { border: 0 } ,  borderColor: '#391f0f'}, {'&.Mui-focused': { border: 1, borderColor: '#391f0f', '.MuiOutlinedInput-notchedOutline': { border: 0 }}},]}
             className='h-20 text-2xl bg-[#fef8ee] block rounded-3xl w-full'
         >
-            {options.map((option, i) => (<MenuItem className='text-2xl' key={i} value={option}>{option}</MenuItem>))}
+            {options.map((option, i) => (
+                <MenuItem className='text-2xl'
+                    sx={{ padding: "13px" ,bgcolor: "#FEF8EE", "&.Mui-selected": {bgcolor: "#F5E8FF",}}}
+                    key={i} value={option}
+                >{option}</MenuItem>
+            ))}
         </Select>
     </FormControl>
   )
