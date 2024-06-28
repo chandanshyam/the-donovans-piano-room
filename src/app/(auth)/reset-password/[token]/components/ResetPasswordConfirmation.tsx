41e8c84509bbd10e4b772d4a05aa189f48328029
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-28 23:35:26 +0100

    fix backtracked deployment errors

--- src/app/(auth)/reset-password/[token]/components/ResetPasswordConfirmation.tsx
+++ src/app/(auth)/reset-password/[token]/components/ResetPasswordConfirmation.tsx
@@ -7,7 +7,7 @@ export default function ResetPasswordConfirmation() {
     return (
         <AuthSucceedWrapper>
             <h1 className="text-4xl md:text-8xl 2xl:text-8xl 4xl:text-9xl leading-tight tracking-tight text-white mb-5 font-montserrat">
-                Hurray! You're in
+                Hurray! You&apos;re in
             </h1>
             <div className="mb-5 2xl:mt-5 2xl:mb-[20px]">
                 <p className="text-white text-lg md:text-xl 2xl:text-2xl 4xl:text-3xl">Your password was successfully changed. You can now login with your new email and new password.</p>    
