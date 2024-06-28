96e889cc835478984dff9bb5cccff46704b06988
aouiniamine <amine.m.aouini@hotmail.com>
2024-06-28 23:22:11 +0100

    fix button styling method

--- src/components/atoms/Button3.tsx
+++ src/components/atoms/Button3.tsx
@@ -1,5 +1,5 @@
-export default function Button1({text, onClick=null, style=''}: {text: string, onClick?: any, style?: string}) {
+export default function Button1({text, onClick=null, style={}}: {text: string, onClick?: any, style?: any}) {
     return (
-        <button onClick={onClick} className={style + ' w-full text-center bg-primary-purple py-3 rounded-3xl text-[12px] 3xl:text-2xl 4xl:text-3xl text-white font-semibold 2xl:py-4 3xl:py-5 2xl:rounded-full'} type='submit'>{text}</button>
+        <button onClick={onClick} className={' w-full text-center bg-primary-purple py-3 rounded-3xl text-[12px] 3xl:text-2xl 4xl:text-3xl text-white font-semibold 2xl:py-4 3xl:py-5 2xl:rounded-full'} type='submit' style={style}>{text}</button>
     )
 }
