260fb411255452f35fd23bee321108b51a0fbaa6
Kavin Karthik <kavinkarthikhbk@gmail.com>
2024-07-03 15:10:42 -0400

    Implemented Hover effects for Buttons

--- src/components/atoms/Button1.tsx
+++ src/components/atoms/Button1.tsx
@@ -1,5 +1,5 @@
 export default function Button1({text, onClick=null}: {text: string, onClick?: any}) {
     return (
-        <button onClick={onClick} className='w-full text-center bg-primary-yellow py-3 rounded-3xl text-[12px] 3xl:text-2xl text-primary-purple font-semibold 2xl:py-4 3xl:py-5 2xl:rounded-full' type='submit'>{text}</button>
+        <button onClick={onClick} className='w-full text-center bg-[#F0D454] hover:bg-[#E9BB18] active:bg-[#DAA718]  py-3 rounded-3xl text-[12px] 3xl:text-2xl text-primary-purple font-semibold 2xl:py-4 3xl:py-5 2xl:rounded-full' type='submit'>{text}</button>
     )
 }
