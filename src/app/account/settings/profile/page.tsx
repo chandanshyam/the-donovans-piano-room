bb8cb5ad8af9b7ae5d8a8f2d0e4840ba668c6d84
Manas Paranjape <manasparanjape29@gmail.com>
2024-07-09 20:30:04 -0400

    added profile page

---
+++ src/app/account/settings/profile/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Calender from "./components/Calender";
+
+export default function page() {
+  return (
+      <div className="flex w-full">
+        <Calender />
+      </div>
+  )
+}
