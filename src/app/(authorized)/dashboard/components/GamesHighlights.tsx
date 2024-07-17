72558e157740513494d20013077cd757f36912c9
aouiniamine <amine.m.aouini@hotmail.com>
2024-07-17 22:00:36 +0100

    finalize dashboard

--- src/app/(authorized)/dashboard/components/GamesHighlights.tsx
+++ src/app/(authorized)/dashboard/components/GamesHighlights.tsx
@@ -11,9 +11,9 @@ export default function GamesHighlights() {
             <div className="relative h-[2.5vh] w-[2.5vh]">
                 <Image src="/dashboard/game-icon.svg" fill alt=""/>
             </div>
-            <p className="text-xl 3xl:text-2xl 4xl:text-3xl font-medium">Games in progress {`(${dummydata.length})`}</p>
+            <p className="text-xl 3xl:text-2xl 4xl:text-3xl font-semibold font-roboto">Games in progress {`(${dummydata.length})`}</p>
         </div>
-        <div className="relative flex overflow-x-scroll w-[96%] ml-[2%] mt-[2.5%] gap-[5%] pb-[1vh] justify-around">
+        <div className="relative flex overflow-x-scroll w-[96%] ml-[2%] mt-[2.5%] gap-[5%] pb-[.8vh] justify-around">
             {dummydata.map((game, i) => (
                 <div key={i} className="group relative flex bg-white mt-[2.3%] h-[11vh] border-2 border-b-[9px] border-[#DDB5FD] hover:border-primary-purple hover:bg-secondary-purple rounded-br-[17px] rounded-bl-[17px] rounded-2xl w-[24vw] items-center cursor-pointer ">
                     <div className="relative h-[7vh] w-[7vh] ml-[5%]">
--- src/app/(authorized)/dashboard/page.tsx
+++ src/app/(authorized)/dashboard/page.tsx
@@ -19,17 +19,18 @@ export default function Page() {
     })
   return (
     <AuthorizedWrapper2 pageTitle={authorizedWrapperTitles.Dashboard} openedLink={nav4leftLinks.dashboard}>
-        <div className="relative flex h-[75vh] mt-[1.5%] overflow-y-auto z-[30]">
+        <div className="relative flex h-[75vh] mt-[1.5%] overflow-y-auto z-[30] gap-[8%]">
             <div className="w-[60%]">
-                <h1 className="text-primary-brown text-6xl 3xl:text-7xl 4xl:text-8xl font-semibold">Morning {displayName}!</h1>
+                <h1 className="text-primary-brown text-6xl 3xl:text-7xl 4xl:text-8xl font-semibold montserrat">Morning {displayName}!</h1>
                 <p className="text-3xl 3xl:text-4xl 4xl:text-5xl mt-[2%]">Check out the latest stuff we have ready for you.</p>
                 <FirstLesson/>
                 <GamesHighlights/>
                 
             </div>
             <div>
-                    <MusicNotes/>
-                </div>
+                <MusicNotes/>
+            </div>
+            
         </div>
     </AuthorizedWrapper2>
   )
