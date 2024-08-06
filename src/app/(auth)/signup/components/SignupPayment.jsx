039b279fcb7e9dad33e564a5ad74d991795585f0
Manas Paranjape <manasparanjape29@gmail.com>
2024-08-06 12:18:47 -0400

    completed signup payment

--- src/app/(auth)/signup/components/SignupPayment.jsx
+++ src/app/(auth)/signup/components/SignupPayment.jsx
@@ -1,6 +1,7 @@
 import { PayPalScriptProvider, PayPalButtons, PayPalNumberField, PayPalExpiryField, PayPalCardFieldsProvider, PayPalCVVField, usePayPalCardFields, PayPalCardFieldsForm, PayPalCardFieldsComponentOptions, CardFieldStyle } from "@paypal/react-paypal-js";
 import Button1 from "@/components/atoms/Button1";
 import { useState } from "react";
+import Link from "next/link";
 import Button2 from "@/components/atoms/Button2";
 import SignupHeader from "./SignupHeader";
 import { useSetAtom } from "jotai";
@@ -9,53 +10,81 @@ export default function SignupPayment() {
     const [displayCardFields, setDisplayCardFields] = useState(false)
     const setSingupStep = useSetAtom(singupStepAtom)
     const fieldStyle = {
+        '.card-icon': {
+            'display': 'none',
+            "padding-bottom": "1rem",
+            "padding-top": "1.75rem",
+        },
+        'body': {
+            "padding": "0",
+        },
         'input': {
+            "appearance": "none",
             "border": "2px solid #391F0F",
             "borderRadius": "15px",
+            "padding-bottom": "1rem",
+            "padding-top": "1.75rem",
+            "margin-left": "0.5rem",
         }, ".invalid": {
             "color": "black",
             "border": "none"
         },
         ":focus": {
-            "border": "none"
+            "border": "2px solid #391F0F",
+            "box-shadow": "none"
         }
     }
-  return (
-    <div className='w-[24vw] 3xl:w-[26vw]'>
-        <SignupHeader navName='Membership' navLink='' stepNum={4} stepName='Add your payment method' onClickNav={()=>setSingupStep(3)} />
-        <PayPalScriptProvider options={{clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID, components: ["buttons", "card-fields"], enableFunding: "venmo"}}>
-            {!displayCardFields && (<>
-                <Button1 style={{padding: "15px", marginBottom: "5%"}} text="Check out with credit card" onClick={()=>{setDisplayCardFields(true)}}></Button1>
-                <PayPalButtons style={{shape: "pill"}}></PayPalButtons>
-            </>)}
 
-            {displayCardFields && 
-                <PayPalCardFieldsProvider createOrder={async()=>{console.log("11");return ""}} onApprove={async()=>''} onError={async()=>''}>
-                {/* <PayPalCardFieldsForm> */}
+    const createOrder = async () => {
+        return "";
+    }
+
+    const onApprove = async () => {
+        return "";
+    }
+
+    const onError = async () => {
+        return "";
+    }
+
+    return (
+        <div className='w-[24vw] 3xl:w-[26vw]'>
+            <SignupHeader navName='Membership' navLink='' stepNum={4} stepName='Add your payment method' onClickNav={()=>setSingupStep(3)} />
+            
+            <PayPalScriptProvider options={{clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID, components: "card-fields,buttons", enableFunding: "venmo"}}>
+                {!displayCardFields && (<>
+                    <Button1 style={{padding: "15px", marginBottom: "4%", marginTop: "3%"}} text="Check out with credit card" onClick={()=>{setDisplayCardFields(true)}}></Button1>
+                    <PayPalButtons style={{shape: "pill"}}></PayPalButtons>
+                </>)}
+
+                {displayCardFields && 
+                (<PayPalCardFieldsProvider createOrder={createOrder} onApprove={onApprove} onError={onError}>
                     <label className="relative w-full">
-                        <span className="absolute top-[16px] left-[15px] z-50 text-[#391F0F] font-medium w-[24vw]">Card number</span>
+                        <span className="absolute top-[8px] left-[25px] z-50 text-[#391F0F] font-medium w-[24vw]">Card number</span>
                         <PayPalNumberField style={fieldStyle} placeholder="" />
                     </label>
-                    <div className="flex gap-[2%]">
-
-                    <label className="relative w-full">
-                        <span className="absolute top-[16px] left-[15px] z-50 text-[#391F0F] font-medium w-[24vw]">Expiration date</span>
-                        <PayPalExpiryField style={fieldStyle} placeholder="" />
-                    </label>
-                    <label className="relative w-full">
-                        <span className="absolute top-[16px] left-[15px] z-50 text-[#391F0F] font-medium w-[24vw]">CVV</span>
-                        <PayPalCVVField style={fieldStyle} placeholder="" />
-                    </label>
+                    <div className="flex gap-[2%] mb-[4%]">
+                        <label className="relative w-full">
+                            <span className="absolute top-[8px] left-[15px] z-50 text-[#391F0F] font-medium w-[24vw]">Expiration date</span>
+                            <PayPalExpiryField style={fieldStyle} placeholder="" />
+                        </label>
+                        <label className="relative w-full">
+                            <span className="absolute top-[8px] left-[15px] z-50 text-[#391F0F] font-medium w-[24vw]">CVC</span>
+                            <PayPalCVVField style={fieldStyle} placeholder="" />
+                        </label>
                     </div>
+                    <label className="flex items-center mb-[4%]">
+                        <input type="checkbox" className="mr-[1rem] accent-primary-yellow" />
+                        <span className="text-[#391F0F] text-white text-lg">Set as default payment method</span>
+                    </label>
                     <SubmitPayment/>
                     <Button2 style={{marginTop: "4%"}} text="Cancel" onClick={()=>setDisplayCardFields(false)}></Button2>
-                    
-                {/* </PayPalCardFieldsForm> */}
                 </PayPalCardFieldsProvider>
-            }
-        </PayPalScriptProvider>
-    </div>
-  )
+            )}
+            </PayPalScriptProvider>
+            <p className='w-full text-center mt-[4vh] text-lg 3xl:text-2xl  text-white bg-primary-purple py-3 rounded-3xl 2xl:rounded-4xl text-[12px] mt-9 2xl:py-5 3xl:py-8'>Already have an account? <Link href="/login" className='text-primary-yellow underline'>Log in</Link></p> 
+        </div>
+    )
 }
 
 const SubmitPayment = () => {
