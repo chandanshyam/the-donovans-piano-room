7800c6aca76964695a6b20aeb2c0fc1655ee083a
Manas Paranjape <manasparanjape29@gmail.com>
2024-08-06 12:18:27 -0400

    fixed import for emailVerificationContent

--- src/app/(auth)/signup/components/AllSignupSteps.tsx
+++ src/app/(auth)/signup/components/AllSignupSteps.tsx
@@ -2,7 +2,7 @@ import { singupStepAtom } from '@/utils/stores'
 import { useAtomValue, useSetAtom } from 'jotai'
 import SignupForm from './SignupForm'
 import SignupMembershipContent from './membership/SignupMembershipContent'
-import EmailVerificationContent from './email-verification/components/EmailVerificationContent.tsx'
+import EmailVerificationContent from './email-verification/components/EmailVerificationContent'
 import SignupPayment from './SignupPayment'
 
 export default function AllSignupSteps() {
