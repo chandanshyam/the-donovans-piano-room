7749b4b798e8ced39d30c2c9215c65781f867db2
Manas Paranjape <manasparanjape29@gmail.com>
2024-08-19 14:40:54 -0400

    set up timer countdown, resend

--- src/app/(auth)/signup/components/email-verification/components/EmailVerificationForm.tsx
+++ src/app/(auth)/signup/components/email-verification/components/EmailVerificationForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined'
 import Button1 from '@/components/atoms/Button1'
 import Button2 from '@/components/atoms/Button2'
@@ -14,12 +14,32 @@ export default function EmailVerificationForm({setToIsVerified}: {setToIsVerifie
         e.preventDefault()
         setToIsVerified()
     }
+    
+    const [timeLeft, setTimeLeft] = useState(600)
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft((prevTime) => prevTime - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    const sendNewCode = () => {
+        setTimeLeft(600)
+    }
+
     return (
         <section>
             <p className='mt-4 text-white text-[13px] 2xl:text-[16px] 3xl:text-[18px] font-montserrat mb-5'>Enter the verification 6 digit-code we sent to jacks@email.com</p>
             <div className='w-full flex py-4 gap-4 justify-center text-center bg-[#FEF8EE] py-3 rounded-xl text-[16px] text-black font-semibold 2xl:py-5 2xl:rounded-xl'>
                 <TimerOutlinedIcon className=' text-3xl' />
-                <p className='text-[13px] 2xl:text-[16px]'>This code expires in 10:00 minutes</p>
+                {timeLeft > 0 ? (
+                    <p className='text-[13px] 2xl:text-[16px]'>This code expires in {Math.floor(timeLeft / 60)}:{timeLeft % 60 < 10 ? `0${timeLeft % 60}` : timeLeft % 60} minutes</p>
+                ) : (
+                    <p className='text-[13px] 2xl:text-[16px]'>The code has expired</p>
+                )}
             </div>
             <form className="mt-8 mb-7 w-full">
                 <p className='text-primary-yellow text-lg 3xl:text-2xl mb-3'>Enter the 6 digit code</p>
@@ -38,7 +58,7 @@ export default function EmailVerificationForm({setToIsVerified}: {setToIsVerifie
                 </div>
                 <Button1 text="Verify" onClick={handleVerify}/>
             </form>
-            <Button2 text='Send a new code' onClick={()=>{}} />
+            <Button2 text='Send a new code' onClick={sendNewCode}/>
         </section>
     )
 }
