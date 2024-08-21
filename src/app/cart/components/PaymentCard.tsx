746f4091e2494068b1cfaedbf06361caf21a509b
aouiniamine <amine.m.aouini@hotmail.com>
2024-08-21 10:03:06 +0100

    calculate added items total

--- src/app/cart/components/PaymentCard.tsx
+++ src/app/cart/components/PaymentCard.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Button3 from "@/components/atoms/Button3";
 import Button4 from "@/components/atoms/Button4";
+import { useAtomValue } from "jotai";
+import { addedCartItemsAtom } from "@/utils/stores";
 
 export default function PaymentCard() {
+  const addedCartItems = useAtomValue(addedCartItemsAtom)
+  const cartItemsPrice = useMemo(()=>{
+    let total = 0.00
+    addedCartItems.forEach(item => {
+      total += parseFloat(item.price) * item.quantity
+    })
+    return total
+  }, [addedCartItems])  
   return (
     <div className="flex flex-col rounded-[12px] bg-white p-[32px] gap-[1vh] shadow-md tablet:w-full laptop:w-[45%] h-full">
       <h4 className="font-roboto text-4xl font-semibold leading-[28px] text-[#59371D]">
@@ -14,7 +24,7 @@ export default function PaymentCard() {
         <div className="flex flex-col gap-[0.5vh]">
           <div className="flex justify-between items-center">
             <p className="font-roboto text-[1.6rem] leading-[24px] text-[#1C1A1A]">Original Price</p>
-            <p className="font-roboto text-[1.6rem] leading-[24px] text-[#1C1A1A]">$25.00</p>
+            <p className="font-roboto text-[1.6rem] leading-[24px] text-[#1C1A1A]">${cartItemsPrice.toFixed(2)}</p>
           </div>
 
           <div className="flex justify-between items-center">
@@ -27,7 +37,7 @@ export default function PaymentCard() {
 
         <div className="flex justify-between items-center font-bold">
           <p className="font-roboto text-[1.6rem] leading-[24px] text-[#1C1A1A]">Total</p>
-          <p className="font-roboto text-[1.6rem] leading-[24px] text-[#1C1A1A]">$28.00</p>
+          <p className="font-roboto text-[1.6rem] leading-[24px] text-[#1C1A1A]">${(cartItemsPrice + 3.00).toFixed(2)}</p>
         </div>
       </div>
 
