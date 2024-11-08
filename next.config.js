ff0eda601204488bdaf7ceab0924d7f96f8f153f
Keshav Mehta <Keshav.Mehta@student.csulb.edu>
2024-11-07 18:50:02 -0800

    checking Vercel deployment

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
       return config;
     },
     async rewrites() {
-      const destinationUrl  = process.env.NODE_ENV=="production"?process.env.BE_PROD_BASE_URL:process.env.BE_BASE_URL 
+      const destinationUrl  = process.env.VERCEL.ENV=="production"?process.env.BE_PROD_BASE_URL:process.env.BE_BASE_URL 
       return [
         {
           source: '/api/:path*',
