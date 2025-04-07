b021951f3af3edbc6cb6c3e8959be195677ad3aa
Keshav Mehta <Keshav.Mehta@student.csulb.edu>
2025-04-07 01:00:10 -0700

    removed build error

--- src/components/atoms/Button2.tsx
+++ src/components/atoms/Button2.tsx
@@ -1,4 +1,4 @@
-export default function Button2({ text, onClick = null, style = {}, disable }: { text: string, onClick?: any, style?: any, disable?: boolean }) {
+export default function Button2({ text, onClick = null, style = {}, disable }: { text:  React.ReactNode, onClick?: any, style?: any, disable?: boolean }) {
   return (
     <button onClick={onClick} disabled = {disable} style={style} className="w-full border border-primary-yellow active:bg-[#FAF5C7] py-3 2xl:py-4 rounded-full text-[12px] 2xl:text-2xl 4xl:text-3xl font-semibold text-[#F1D454] cursor-pointer">{text}</button>
   )
