0f302c0e41c2d8f85b5c4050015d9583fcdace8d
Sai Vivek Vangaveti <saivivek116@gmail.com>
2025-04-27 20:11:04 -0700

    lessons page scaffolding

---
+++ src/app/(authorized)/lessons/components/LiveSessionsComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LiveSessionsComponent() {
+    return (
+        <h2 className="text-4xl font-medium text-primary-brown mb-4">
+            Live Session
+        </h2>
+    )
+}
+
+export default LiveSessionsComponent
