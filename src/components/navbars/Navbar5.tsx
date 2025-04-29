59d88f7261da061a6286005c22fee80d6bbc4e44
atharvasalehittal <atharva.salehittal@gmail.com>
2025-04-29 10:49:22 -0400

    Task-1 Contact Issue Fixed

--- src/components/navbars/Navbar5.tsx
+++ src/components/navbars/Navbar5.tsx
@@ -68,7 +68,7 @@ export default function Navbar5({ page }: { page: string }) {
             <Link 
               className='text-primary-purple hover:text-[#E98427] active:text-[#Da6a1c] text-xl 2xl:text-3xl font-bold flex items-center relative' 
               style={navigationPages.contact === page ? highlightLink: {}} 
-              href="/contact-us"
+              href="#"
               >
               <p>CONTACT</p>
              {navigationPages.contact === page && displayBorder()}
