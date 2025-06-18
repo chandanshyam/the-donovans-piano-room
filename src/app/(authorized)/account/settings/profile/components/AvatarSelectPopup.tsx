af2178732dab309638bfe2bc4961f545b89d3912
Jiaqi-Ye <jye224@wisc.edu>
2025-06-19 03:25:38 +0800

    change to new avatar

--- src/app/(authorized)/account/settings/profile/components/AvatarSelectPopup.tsx
+++ src/app/(authorized)/account/settings/profile/components/AvatarSelectPopup.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import { updateUser } from '@/lib/api/userService';
 
-export default function AvatarSelectPopup({avatar, setAvatar, closeSelectingAvatar}: {avatar: string, setAvatar: any, closeSelectingAvatar: any}) {
+export default function AvatarSelectPopup({ avatar, setAvatar, closeSelectingAvatar }: { avatar: string, setAvatar: any, closeSelectingAvatar: any }) {
   const [selectedAvatar, setSelectedAvatar] = useState(avatar)
   const setProfile = useSetAtom(profileAtom)
   const profile = useAtomValue(profileAtom)
@@ -22,11 +22,11 @@ export default function AvatarSelectPopup({avatar, setAvatar, closeSelectingAvat
       document.removeEventListener('click', handleClickOutside);
     };
   });
-  const submitNewAvatar = async() => {
-    const {data, ok} = await updateUser({picture: selectedAvatar, email: profile.email});
-    if(ok){
+  const submitNewAvatar = async () => {
+    const { data, ok } = await updateUser({ picture: selectedAvatar, email: profile.email });
+    if (ok) {
       setProfile((prev: any) => {
-        const newProfile = {...prev, picture: selectedAvatar} 
+        const newProfile = { ...prev, picture: selectedAvatar }
         return newProfile
       })
     }
@@ -34,37 +34,47 @@ export default function AvatarSelectPopup({avatar, setAvatar, closeSelectingAvat
   }
   return (
     <div className='absolute w-[100vw] h-[100vh] z-[220] bg-[black] bg-opacity-50 top-[-9.7vh] left-[-14.5vw]'>
-      <div ref={popupRef} className="absolute w-[30vw] h-[40vh] bg-primary-skin rounded-2xl bottom-[15vh] right-[5vw] px-[2vw] py-[3vh]" style={avatar !== selectedAvatar ? {height: "47vh"} : {}}>
+      <div ref={popupRef} className="absolute w-[30vw] h-[60vh] bg-primary-skin rounded-2xl bottom-[15vh] right-[5vw] px-[2vw] py-[3vh]" style={avatar !== selectedAvatar ? { height: "60vh" } : {}}>
         <div className="flex justify-between">
           <p className='text-3xl 3xl:text-4xl 4xl:text-5xl'>Select your avatar</p>
-          <CloseIcon className='text-4xl 3xl:text-5xl 4xl:text-6xl' onClick={closeSelectingAvatar}/>
+          <CloseIcon className='text-4xl 3xl:text-5xl 4xl:text-6xl' onClick={closeSelectingAvatar} />
         </div>
         <div className='grid grid-cols-3 grid-rows-2 mt-[3vh] gap-y-6'>
-          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[2])} style={avatars[2] == selectedAvatar ? {borderWidth: "3px"} : {}}>
-            <Image src="/ToBeRemoved/avatar/2.svg" fill alt='' />
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[1])} style={avatars[1] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar default.svg" fill alt='' />
           </div>
 
-          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[3])} style={avatars[3] == selectedAvatar ? {borderWidth: "3px"} : {}}>
-            <Image src="/ToBeRemoved/avatar/3.svg" fill alt='' />
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[2])} style={avatars[2] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar 2.svg" fill alt='' />
           </div>
 
-          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[4])} style={avatars[4] == selectedAvatar ? {borderWidth: "3px"} : {}}>
-            <Image src="/ToBeRemoved/avatar/4.svg" fill alt='' />
-          </div>
-          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[1])} style={avatars[1] == selectedAvatar ? {borderWidth: "3px"} : {}}>
-            <Image src="/ToBeRemoved/avatar/1.svg" fill alt='' />
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[3])} style={avatars[3] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar 3.svg" fill alt='' />
           </div>
 
-          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[5])} style={avatars[5] == selectedAvatar ? {borderWidth: "3px"} : {}}>
-            <Image src="/ToBeRemoved/avatar/5.svg" fill alt='' />
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[4])} style={avatars[4] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar 4.svg" fill alt='' />
           </div>
 
-          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[6])} style={avatars[6] == selectedAvatar ? {borderWidth: "3px"} : {}}>
-            <Image src="/ToBeRemoved/avatar/6.svg" fill alt='' />
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[5])} style={avatars[5] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar 5.svg" fill alt='' />
           </div>
+
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[6])} style={avatars[6] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar 6.svg" fill alt='' />
+          </div>
+
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[8])} style={avatars[8] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar container 8.svg" fill alt='' />
+          </div>
+
+          <div className='relative w-[13vh] h-[13vh] rounded-full border-primary-purple' onClick={() => setSelectedAvatar(avatars[7])} style={avatars[7] == selectedAvatar ? { borderWidth: "3px" } : {}}>
+            <Image src="/profile/Settings/Avatar 7.svg" fill alt='' />
+          </div>
+
         </div>
         {selectedAvatar !== avatar && <div className='flex w-full justify-end'>
-          <Button3 text='Save changes' style={{marginTop: "18px", width: "10vw"}} onClick={submitNewAvatar}/>
+          <Button3 text='Save changes' style={{ marginTop: "10px", width: "9vw" }} onClick={submitNewAvatar} />
 
         </div>}
       </div>
@@ -73,10 +83,12 @@ export default function AvatarSelectPopup({avatar, setAvatar, closeSelectingAvat
 }
 
 const avatars = {
-  1: "/ToBeRemoved/avatar/1.svg",
-  2: "/ToBeRemoved/avatar/2.svg",
-  3: "/ToBeRemoved/avatar/3.svg",
-  4: "/ToBeRemoved/avatar/4.svg",
-  5: "/ToBeRemoved/avatar/5.svg",
-  6: "/ToBeRemoved/avatar/6.svg"
+  1: "/profile/Settings/Avatar default.svg",
+  2: "/profile/Settings/Avatar 2.svg",
+  3: "/profile/Settings/Avatar 3.svg",
+  4: "/profile/Settings/Avatar 4.svg",
+  5: "/profile/Settings/Avatar 5.svg",
+  6: "/profile/Settings/Avatar 6.svg",
+  7: "/profile/Settings/Avatar 7.svg",
+  8: "/profile/Settings/Avatar container 8.svg",
 }
