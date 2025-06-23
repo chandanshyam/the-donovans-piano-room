7007b169c8fb57ecf60a5461967f16774881d98a
Sai Vivek Vangaveti <saivivek116@gmail.com>
2025-06-23 08:54:23 -0700

    Fixed page unresponsive blocker and links

--- src/components/atoms/AccountAndSettingsNav.tsx
+++ src/components/atoms/AccountAndSettingsNav.tsx
@@ -12,12 +12,12 @@ export default function AccountAndSettingsNav({currentPage}: {currentPage: strin
             style={currentPage === settingsNavigation.profile ? {background: "#D8BCFD", fontWeight: "600", color: "#6F219E"} : {}}
           >Profile</p>
         </Link>
-        <Link href="">
+        <Link href="#">
           <p className="text-primary-gray text-xl 3xl:text-2xl 4xl:text-3xl py-[.5vh] px-[1vw] rounded-xl"
           style={currentPage === settingsNavigation.membership ? {background: "#D8BCFD", fontWeight: "600", color: "#6F219E"} : {}}
           >Membership</p>
         </Link>
-        <Link href="">
+        <Link href="#">
           <p className="text-primary-gray text-xl 3xl:text-2xl 4xl:text-3xl py-[.5vh] px-[1vw] rounded-xl"
           style={currentPage === settingsNavigation.paymentMethods ? {background: "#D8BCFD", fontWeight: "600", color: "#6F219E"} : {}}
           >Payment methods</p>
@@ -29,7 +29,7 @@ export default function AccountAndSettingsNav({currentPage}: {currentPage: strin
           {hasUnread && <div className="absolute h-[1.1vh] w-[1.1vh] bg-[#C2370C] rounded-full top-[-.3vh] right-[-.2vw]"></div>}
           </p>
         </Link>
-        <Link href="">
+        <Link href="#">
           <p className="text-primary-gray text-xl 3xl:text-2xl 4xl:text-3xl py-[.5vh] px-[1vw] rounded-xl"
           style={currentPage === settingsNavigation.timeSpent ? {background: "#D8BCFD", fontWeight: "600", color: "#6F219E"} : {}}
           >Time spent</p>
